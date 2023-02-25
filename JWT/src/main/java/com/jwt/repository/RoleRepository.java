8fb513f299bd8cd94c603a6e1e0b4262c00eb46a
Vanshika2499 <111451412+Vanshika2499@users.noreply.github.com>
2023-02-25 19:44:05 +0530

    Add files via upload

---
+++ JWT/src/main/java/com/jwt/repository/RoleRepository.java
@@ -0,0 +1,16 @@
+package com.jwt.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jwt.models.ERole;
+import com.jwt.models.Role;
+
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long>{
+Optional<Role> findByName(ERole name);
+
+}
